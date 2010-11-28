5133b94e808249b30d2b3446aaf680b5fddf2579
Nicholas Kostelnik <nick@blackartstudios.net>
2010-11-28 02:10:51 +0000

    start of multicast detection

--- src/shared/NixSocket.h
+++ src/shared/NixSocket.h
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -25,6 +26,9 @@
 #include <vector>
 
 #include "ISocket.hpp"
+
+#define WORMHOLE_GROUP "225.0.0.37"
+#define MSGBUFSIZE 256
 
 class NixSocket  : public ISocket
 {
@@ -35,18 +39,29 @@ public:
 	
 	void listen_(unsigned int port)
 	{
-		listen_sock_ = create_socket(port);
+		listen_sock_ = create_listen_socket(port);
 		max_socket_ = listen_sock_;
 		FD_SET(listen_sock_, &read_sockets_);
-		
+    set_reuse_port(listen_sock_);
 		listen(listen_sock_, 5);
+    
+    multicast_sock_ = create_multicast_socket(port);
 		
 		std::clog << "listening on port: " << port << std::endl;
 	}
+  
+  int create_multicast_socket(unsigned int port)
+  {
+		int sock = open_socket(SOCK_DGRAM);
+    set_non_blocking(sock);
+		bind_socket(sock, port);    
+    set_multicast(sock);
+		return sock;    
+  }
 	
-	int open_socket()
+	int open_socket(int type)
 	{    
-		int sock = socket(AF_INET, SOCK_STREAM, 0);
+		int sock = socket(AF_INET, type, 0);
 		
 		if (sock < 0)
 		{
@@ -69,18 +84,87 @@ public:
 		}
 	}		
 	
-	
-	int create_socket(unsigned int port)
+  void set_multicast(int sock)
+  {
+    struct ip_mreq mreq;
+    mreq.imr_multiaddr.s_addr = inet_addr(WORMHOLE_GROUP);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+    if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+      std::cerr << "ERROR setting up multicast" << std::endl;
+    }
+  }
+  
+  void set_reuse_port(int sock)
 	{
-		int sock = open_socket();
+    u_int yes = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+      std::cerr << "ERROR reusing ADDR" << std::endl;
+    }
+  }
+  
+	int create_listen_socket(unsigned int port)
+	{
+		int sock = open_socket(SOCK_STREAM);
 		set_non_blocking(sock);
-		bind_socket(sock, port);
-		
+		bind_socket(sock, port);    
 		return sock;    
 	}
+  
+  std::string hostname()
+  {
+    struct addrinfo hints, *info;
+    int gai_result;
+    
+    char hostname[1024];
+    hostname[1023] = '\0';
+    gethostname(hostname, 1023);
+    
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC; /*either IPV4 or IPV6*/
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_CANONNAME;
+    
+    if ((gai_result = getaddrinfo(hostname, "http", &hints, &info)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai_result));
+      exit(1);
+    }
+    
+    return info->ai_next->ai_canonname;
+  }
 	
 	received_data* receive()
 	{
+    struct sockaddr_in addr;  
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(1723);
+    socklen_t addrlen = sizeof(addr);
+    
+    char multicast_buffer[10000];
+    if (recvfrom(multicast_sock_, multicast_buffer, MSGBUFSIZE, 0, (struct sockaddr *) &addr, &addrlen) < 0)
+    {  
+      if (errno == EWOULDBLOCK)
+      {
+//        std::cerr << "would block" << std::endl;
+      }
+      else {
+        std::cerr << "ERROR reading from multicast group" << std::endl;
+      }
+    }
+    else {
+
+      struct sockaddr_in addr;  
+      memset(&addr, 0, sizeof(addr));
+      addr.sin_family = AF_INET;
+      addr.sin_addr.s_addr = inet_addr(WORMHOLE_GROUP);
+      addr.sin_port = htons(1723);
+      
+      std::clog << hostname() << std::endl;
+      sendto(multicast_sock_, hostname().c_str(), sizeof(hostname().c_str()), 0, (struct sockaddr *) &addr, addrlen);
+    }
+
+  
 		received_data* return_data = new received_data();
 		
 		struct timeval timeout;
@@ -121,7 +205,7 @@ public:
 						char* buffer = new char[256];
 						
 						int result = read(i, buffer, 256);
-						
+            
 						if (result < 0)
 						{ 
 							std::cerr << "ERROR reading from socket: " << strerror(errno) << std::endl;
@@ -135,7 +219,8 @@ public:
 						}
 						
 						if (result > 0)
-						{							
+						{
+              std::clog << buffer << std::endl;
 							return_data->push_back(buffer);
 						}
 					}
@@ -263,6 +348,7 @@ private:
 	
 	int max_socket_;
 	int listen_sock_;
+  int multicast_sock_;
 	fd_set read_sockets_;
 	
 };

684eb8ce69ca3217ad1c1b86b906afd397d6fd53
Nicholas Kostelnik <nick@blackartstudios.net>
2010-12-07 11:00:51 +0000

    raised port numbers

--- src/shared/Constants.hpp
+++ src/shared/Constants.hpp
@@ -1,9 +1,9 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
-	static const unsigned int SERVER_PORT = 1723;
-  static const unsigned int CLIENT_LISTEN_PORT = 1724;
-  static const unsigned int CLIENT_SEND_PORT = 1725;
+	static const unsigned int SERVER_PORT = 44195;
+  static const unsigned int CLIENT_LISTEN_PORT = 44196;
+  static const unsigned int CLIENT_SEND_PORT = 44197;
 	static const unsigned int DOUBLE_CLICK_THRESHOLD = 300;
 	static const unsigned int TIME_OUT = 5000;
 
